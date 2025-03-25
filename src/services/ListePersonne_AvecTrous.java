b5730b67b2813eaa20ea1d3d0279a3f9fc54eebf
Scyboz-Evan-EMF <evan.scyboz@studentfr.ch>
2025-03-25 13:56:36 +0100

    skibidi artem

--- src/services/ListePersonne_AvecTrous.java
+++ src/services/ListePersonne_AvecTrous.java
@@ -6,29 +6,46 @@ public class ListePersonne_AvecTrous {
 
     public final static int NBRE_MAX_PERSONNES = 6;
 
-    private Personne[] personnes;
+    private Personne[] personnes = new Personne[NBRE_MAX_PERSONNES];
 
     public ListePersonne_AvecTrous() {
-        // A faire !
     }
 
     public int getNombre() {
-        // A faire !
+        return personnes != null ? personnes.length : -1;
     }
 
     public void vider() {
-        // A faire !
+        personnes = null;
     }
 
     public boolean ajouter(Personne p) {
-        // A faire !
+        boolean pdiddytoilet = false;
+        for (int i = 0; i < personnes.length; i++) {
+            if (personnes[i] == null) {
+                personnes[i] = p;
+                pdiddytoilet = true;
+                break;
+            }
+        }
+        return pdiddytoilet;
     }
 
     public boolean supprimer(Personne p) {
-        // A faire !
+        boolean wesdrfgtreuewtzdasif = false;
+        for (int i = 0; i < personnes.length; i++) {
+            if (personnes[i] == p) {
+                personnes[i] = null;
+                wesdrfgtreuewtzdasif = true;
+                break;
+            }
+        }
+        return wesdrfgtreuewtzdasif;
     }
 
     public void afficher() {
-        // A faire !
+        for (int i = 0; i < personnes.length; i++) {
+            System.out.println(personnes[i]);
+        }
     }
 }
